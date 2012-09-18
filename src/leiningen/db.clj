04eeb76415cf2e59602d0cd8af56f5acda15d757
reborg <reborg@reborg.net>
2012-09-18 18:07:48 +0100

    generified the plug in requiring a generic namespace. Added branches for different tasks in isolation. Required: refactoring.

--- src/leiningen/db.clj
+++ src/leiningen/db.clj
@@ -1,6 +1,36 @@
-(ns leiningen.db)
+(ns leiningen.db
+  (:use [leiningen.core.eval :only [eval-in-project]]))
 
 (defn db
-  "I don't do a lot."
+  "Entry point for all database setup tasks."
   [project & args]
-  (println "Hi!"))
+  (let [task (first args)]
+    (cond 
+      (= nil task)
+      (do
+        (println "Executing drop-tables, migrate and seed")
+        (eval-in-project project 
+                         '(do 
+                            (db/drop-tables)
+                            (db/migrate)
+                            (db/seed))
+                         '(require '[leindb :as db])))
+      (= "drop-tables" task)
+      (do
+        (println "Invoking leindb/drop-tables")
+        (eval-in-project project 
+                         '(db/drop-tables)
+                         '(require '[leindb :as db])))
+      (= "migrate" task)
+      (do
+        (println "Invoking leindb/migrate")
+        (eval-in-project project 
+                         '(db/migrate)
+                         '(require '[leindb :as db])))
+      (= "seed" task)
+      (do
+        (println "Invoking leindb/seed")
+        (eval-in-project project 
+                         '(db/seed)
+                         '(require '[leindb :as db])))
+      :else (println "Lein-db: Unkown task"))))
